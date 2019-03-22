aa14a72cc8cff89e693a5bcb477a9310418e2bfd
Marian ALDESCU <marian.aldescu@igtp4-mac04.isim.intra>
2019-03-22 11:32:35 +0100

    intro

---
+++ ProjetSwift/ProjetSwift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ProjetSwift
+//
+//  Created by Marian ALDESCU on 22/03/2019.
+//  Copyright © 2019 Marian ALDESCU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
