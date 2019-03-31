6996a7ddcb2c39ab518f9bde4eb354b3f9521981
user152227 <user152227@87-237-62-13.rdns.melbourne.co.uk>
2019-03-31 14:48:11 +0100

    NewDepense almost done

---
+++ ProjetSwift/ProjetSwift/Models/AssociationDepensePayer.swift
@@ -0,0 +1,35 @@
+//
+//  AssociationDepensePerson.swift
+//  ProjetSwift
+//
+//  Created by user152227 on 3/31/19.
+//  Copyright © 2019 Marian ALDESCU. All rights reserved.
+//
+
+import Foundation
+
+extension AssociationDepensePayer {
+    
+    var sum : Float {
+        get { return self.sumPaid }
+        set { self.sumPaid = newValue }
+    }
+    
+    var nameD : String {
+        get { return self.nameDepense }
+        set { self.nameDepense = newValue }
+    }
+    
+    var nameP : String {
+        get { return self.nameDepense }
+        set { self.nameDepense = newValue }
+    }
+    
+    var nameP : String {
+        get { return self.nameDepense }
+        set { self.nameDepense = newValue }
+    }
+    
+    
+}
+
---
+++ ProjetSwift/ProjetSwift/New Group/NewObjectControllers/NewDepenseViewController.swift
@@ -0,0 +1,13 @@
+//
+//  NewDepenseViewController.swift
+//  ProjetSwift
+//
+//  Created by user152227 on 3/31/19.
+//  Copyright © 2019 Marian ALDESCU. All rights reserved.
+//
+
+import UIKit
+
+class NewDepenseViewController: NSObject {
+
+}
